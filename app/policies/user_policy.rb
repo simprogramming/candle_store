8747515f3784356cf75bdf3a750b6919acf01125
SimSimSim <leivaprogramming@gmail.com>
2023-07-10 21:10:40 -0400

    last touch up?

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,6 +2,6 @@ class UserPolicy < ApplicationPolicy
   include AdminBasePolicy
 
   def permitted_attributes
-    %i[first_name last_name email role password password_confirmation photo password]
+    %i[first_name last_name email password password_confirmation]
   end
 end
